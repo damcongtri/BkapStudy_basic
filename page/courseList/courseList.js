6a45909a2682660bc34919ee0673997f5f76de49
DESKTOP-4OKI60B\trid2 <trid2300@gmail.com>
2022-09-27 11:10:17 +0700

    ok

--- page/courseList/courseList.js
+++ page/courseList/courseList.js
@@ -1,3 +1,17 @@
 function changePrice() {
     console.log(document.getElementsByClassName('price-range')[0].value);
 }
+function rangeSlideMin(value) {
+    document.getElementById('rangeValueMin').innerHTML = value;
+}
+function rangeSlideDate(value) {
+    document.getElementById('rangeValueDate').innerHTML = value;
+}
+function rangeSlideMax(value) {
+
+    if (value == 1000) {
+        document.getElementById('rangeValueMax').innerHTML = value + '+';
+    } else {
+        document.getElementById('rangeValueMax').innerHTML = value
+    }
+}
